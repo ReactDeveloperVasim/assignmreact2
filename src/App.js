d7c819df8d933a4606836de9c20f7cf3e1123c6b
ReactDeveloperVasim <reactdevloper.vasim@gmail.com>
2023-10-18 16:22:05 +0530

    assignReact2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Css } from "./component/Css";
+import { Html } from "./component/Html";
+import { Java } from "./component/Java";
+import { Reactjs } from "./component/Reactjs";
+import "./component/Style.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Html />
+      <Css />
+      <Java />
+      <Reactjs />
     </div>
   );
 }
---
+++ src/component/Java.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Java=()=>{
+    return(
+        <div className="sub">
+            <h1>JAVASCRIPT</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad id aspernatur maiores quia ipsum ab delectus animi, facilis, cum minus consequuntur, quas eaque voluptates quae aut ipsam reprehenderit unde numquam facere officia omnis officiis quibusdam. Veniam qui, ea quo doloremque ipsa repellat aliquam voluptate atque tempore similique, recusandae officiis sit cum facilis rerum odit rem consectetur suscipit nobis ad corrupti? Animi dolorem omnis necessitatibus atque aut. Praesentium esse iure velit, sit perspiciatis quibusdam dicta ipsam cumque molestiae tenetur modi dolores nihil temporibus earum aliquid excepturi, minus harum explicabo voluptas accusantium distinctio suscipit, deleniti animi? Necessitatibus culpa impedit eius itaque quod.</p>
+        </div>
+    );
+}
